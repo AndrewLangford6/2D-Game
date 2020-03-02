446b5ceef9d14ced1af7dabab5ae8d2798eafd5b
Andrew Langford <Andrew.Langford.6@gmail.com>
2020-03-02 13:36:37 -0500

    Add project files.

---
+++ 2D Game/Hero.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _2D_Game
+{
+    class Hero
+    {
+        public SolidBrush heroBrush;
+        public int x, y, size;
+        Random randGen = new Random();
+
+        public Hero(int _x, int _y, int _size)
+        {
+            x = _x;
+            y = _y;
+            size = _size;
+
+            int randValue = randGen.Next(1, 3);
+
+            if (randValue == 1)
+            {
+                heroBrush = new SolidBrush(Color.Red);
+            }
+            else if (randValue == 2)
+            {
+                heroBrush = new SolidBrush(Color.Orange);
+            }
+        }
+        public void Move(string direction)
+        {
+            if (direction == "left")
+            {
+                x = x - 5;
+            }
+
+            if (direction == "right")
+            {
+                x = x + 5;
+            }
+
+        }
+    }
+}
---
+++ 2D Game/MainScreen.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2D_Game
+{
+    public partial class MainScreen : UserControl
+    {
+
+        Boolean leftArrowDown, rightArrowDown;
+
+        int hSize = 30;
+
+
+
+        Hero hero;
+
+        public MainScreen()
+        {
+            InitializeComponent();
+            OnStart();
+        }
+
+        public void OnStart()
+        {
+            hero = new Hero(Width / 2 - hSize / 2, 400, hSize);
+        }
+
+        private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            //player 1 button presses
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+            }
+        }
+
+        private void GameScreen_KeyUp(object sender, KeyEventArgs e)
+        {
+            //player 1 button releases
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+            }
+        }
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            if (leftArrowDown)
+            {
+                hero.Move("left");
+            }
+
+            if (rightArrowDown)
+            {
+                hero.Move("right");
+            }
+
+            Refresh();
+        }
+        public void GameScreen_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(hero.heroBrush, hero.x, hero.y, hero.size, hero.size);
+        }
+    }
+}
